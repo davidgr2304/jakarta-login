8e4143fb7ef4915eee5de63e0ee74b950fdfcd6a
davidgr2304 <davidgr2304@outlook.com>
2026-09-23 21:17:08 +0200

    Segundo commit de jakkarta-login: -    Agregado el método doPost(). -    Agregado error.jsp -    Completado formulario.jsp

--- src/main/java/es/daw/jakartalogin/AltaServlet.java
+++ src/main/java/es/daw/jakartalogin/AltaServlet.java
@@ -19,18 +19,32 @@ public class AltaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         //Leer el fichero de texto tegnologías.txt y cargar en un ArrayList
-        List<String> tecnologias = leerFichero("/WEB-INF/datos/tecnologias.txt");
-
+        List<String> tecnologias = new ArrayList<>();
+        try {
+            tecnologias = leerFichero("/WEB-INF/datos/tecnologias.txt");
+        } catch (IOException e) {
+            request.setAttribute("mensajeError", e.getMessage());
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+        request.setAttribute("tecnologias", tecnologias);
+        request.getRequestDispatcher("/formulario.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String nombre = request.getParameter("nombre");
+        String email = request.getParameter("email");
+        String tecnologia= request.getParameter("tecnologia");
+        String nivel = request.getParameter("nivel");
 
+        request.setAttribute("tecnologia", tecnologia);
+        request.setAttribute("nombre", nombre);
+        request.setAttribute("email", email);
+        request.setAttribute("nivel", nivel);
 
-
+        request.getRequestDispatcher("/confirmacion.jsp").forward(request, response);
     }
 
     private List<String> leerFichero(String rutaFichero) throws IOException {
@@ -39,6 +53,7 @@ public class AltaServlet extends HttpServlet {
         InputStream is = getServletContext().getResourceAsStream(rutaFichero);
 
         if (is == null) {
+            //TODO Trabajar con excepciones propias
             throw new IOException("No se encuentra el fichero "+rutaFichero);
         }
 
